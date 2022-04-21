181188ba823859808fa8edd0bfda3770fa11b918
Martín <magadiflo@gmail.com>
2022-04-20 23:54:54 -0500

    Agregando más datos en el token JWT

--- src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.app.auth.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,12 +15,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -54,9 +58,18 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
 		
+		Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+		Claims claims = Jwts.claims();
+		claims.put("authorities", new ObjectMapper().writeValueAsString(roles)); //new ObjectMapper().writeValueAsString(roles), convertimos el objeto roles a un formato JSON
+		
 		String username = ((User) authResult.getPrincipal()).getUsername();
-		String token = Jwts.builder().setSubject(username)
-				.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.12345".getBytes()).compact();
+		String token = Jwts.builder()
+				.setClaims(claims)
+				.setSubject(username)
+				.signWith(SignatureAlgorithm.HS512, "Alguna.Clave.Secreta.12345".getBytes())
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 3600000L))
+				.compact();
 
 		/**
 		 * Importante que el nombre del paráemtro sea Authorization y el 
